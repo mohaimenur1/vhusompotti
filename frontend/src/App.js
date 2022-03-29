0ad8869fae08558bbeccf84546bcccd64a1cec16
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-03-29 13:03:58 +0600

    fixing navbar and add link to fileupload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 /** @format */
 
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Error from './pages/Error/Error';
-import LandingPage from './pages/LandingPage';
-import LogIn from './pages/SignIn/LogIn';
-import SignUp from './pages/Register/SignUp';
-import Dashboard from './pages/Dashboard/Dashboard';
-import PostSelling from './pages/Post/PostSelling';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error/Error";
+import LandingPage from "./pages/LandingPage";
+import LogIn from "./pages/SignIn/LogIn";
+import SignUp from "./pages/Register/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import PostSelling from "./pages/Post/PostSelling";
+import Fileupload from "./pages/Fileupload/Fileupload";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<LogIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/post' element={<PostSelling />} />
-        <Route path='*' element={<Error />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/post" element={<PostSelling />} />
+        <Route path="/fileupload" element={<Fileupload />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Dashboard/SideBar/SideBar.js
+++ frontend/src/pages/Dashboard/SideBar/SideBar.js
@@ -1,8 +1,8 @@
 /** @format */
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './SideBar2.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SideBar2.css";
 // import './SideBar.css';
 const SideBar = () => {
   return (
@@ -56,50 +56,50 @@ const SideBar = () => {
           </div>
         </nav>
       </div> */}
-      <div class='side-menu'>
+      <div class="side-menu">
         {/* <div class='brand-name'>
           <h1>Brand</h1>
         </div> */}
         <ul>
-          <li className='nav-item'>
-            <a className='nav-link active' href='#'>
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
               <span>
-                {' '}
-                <i className='fas fa-border-all'></i>
+                {" "}
+                <i className="fas fa-border-all"></i>
               </span>
-              Dashboard <span className='sr-only'>(current)</span>
+              Dashboard <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span>
-                <i className='fas fa-user-shield'></i>
+                <i className="fas fa-user-shield"></i>
               </span>
               My Files
             </a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span>
-                <i className='fas fa-cloud-upload-alt'></i>
+                <i className="fas fa-cloud-upload-alt"></i>
               </span>
-              File Upload
+              <Link to="/fileupload">File Upload</Link>
             </a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span>
-                <i className='fas fa-headset'></i>
+                <i className="fas fa-headset"></i>
               </span>
               Talk With Expart
             </a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               <span>
-                <i className='far fa-address-card'></i>
+                <i className="far fa-address-card"></i>
               </span>
-              <Link to='/post'>Selling Post</Link>
+              <Link to="/post">Selling Post</Link>
             </a>
           </li>
         </ul>
